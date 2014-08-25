62da8068bb3cd87d2180933855ba80f2eadb6824
Daisuke Masuyama <masuyama.daisuke@moneyforward.co.jp>
2014-08-25 21:08:12 +0900

    全角文字にも対応するようにした。絵文字対応した。ただ、絵文字はそのままカーソルで選択できない糞仕様

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-$('#_chatText').on('keyup', function () {
-  $text_area = $(this);
-  if ( !/@\s/.test( $text_area.val() ) ) {
-    return true;
+var input_pattern_map = {
+  to: /[@＠][\s　]/, 
+  emotion: /[:：][\s　]/
+};
+
+$text_area = $('#_chatText')
+
+var shortcuts = {
+  to: function () {
+    console.log(input_pattern_map.to);
+    if ( !input_pattern_map.to.test( $text_area.val() ) ) {
+      return true;
+    }
+
+    $text_area.val( $text_area.val().replace(input_pattern_map.to, '') );
+    $('#_to').trigger('click');
+  }, 
+  emotion: function () {
+    if ( !input_pattern_map.emotion.test( $text_area.val() ) ) {
+      return true;
+    }
+
+    $text_area.val( $text_area.val().replace(input_pattern_map.emotion, '') );
+    $('#_emoticon').trigger('click');
   }
-  $text_area.val( $text_area.val().replace(/@\s/, '') );
-  $('#_to').trigger('click');
+};
+
+$text_area.on('keyup', function () {
+  for (pattern in input_pattern_map) {
+    shortcuts[pattern]();
+  };
 });
