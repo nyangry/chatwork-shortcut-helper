71160217e8dd0d414187d997894155a07e672d03
nyangry <iacta.alea.esto@gmail.com>
2014-10-17 18:56:01 +0900

    いろいろ
    - gulpを導入して開発のcoffeescript化
    - ショートカットキー追加 タスク追加を（+＋ ）とスペースでできるように
    - お弁当の発注集計を簡単にした

---
+++ gulpfile.coffee
@@ -0,0 +1,14 @@
+gulp   = require('gulp')
+coffee = require('gulp-coffee')
+uglify = require('gulp-uglify')
+
+gulp.task 'js', ->
+  gulp.src('main.coffee')
+      .pipe coffee()
+      .pipe gulp.dest('')
+
+gulp.task 'watch', ->
+  gulp.watch 'main.coffee', ['js']
+
+gulp.task 'default', ->
+  gulp.start 'js'
--- main.js
+++ main.js
@@ -1,32 +1,92 @@
-var input_pattern_map = {
-  to: /[@＠][\s　]/, 
-  emotion: /[:：][\s　]/
-};
+(function() {
+  var $text_area, util,
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-$text_area = $('#_chatText')
-
-var shortcuts = {
-  to: function () {
-    console.log(input_pattern_map.to);
-    if ( !input_pattern_map.to.test( $text_area.val() ) ) {
-      return true;
+  util = {
+    startOfDayTimeStamp: function() {
+      var now, startOfDay, timestamp;
+      now = new Date();
+      startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      timestamp = startOfDay / 1000;
+      return timestamp;
     }
-
-    $text_area.val( $text_area.val().replace(input_pattern_map.to, '') );
-    $('#_to').trigger('click');
-  }, 
-  emotion: function () {
-    if ( !input_pattern_map.emotion.test( $text_area.val() ) ) {
-      return true;
-    }
-
-    $text_area.val( $text_area.val().replace(input_pattern_map.emotion, '') );
-    $('#_emoticon').trigger('click');
-  }
-};
-
-$text_area.on('keyup', function () {
-  for (pattern in input_pattern_map) {
-    shortcuts[pattern]();
   };
-});
+
+  $text_area = $("#_chatText");
+
+  (function() {
+    var input_pattern_map, shortcuts;
+    input_pattern_map = {
+      to: /[@＠][\s　]/,
+      emotion: /[:：][\s　]/,
+      task: /[+＋][\s　]/
+    };
+    shortcuts = {
+      to: function() {
+        if (!input_pattern_map.to.test($text_area.val())) {
+          return true;
+        }
+        $text_area.val($text_area.val().replace(input_pattern_map.to, ""));
+        return $("#_to").trigger("click");
+      },
+      emotion: function() {
+        if (!input_pattern_map.emotion.test($text_area.val())) {
+          return true;
+        }
+        $text_area.val($text_area.val().replace(input_pattern_map.emotion, ""));
+        return $("#_emoticon").trigger("click");
+      },
+      task: function() {
+        if (!input_pattern_map.task.test($text_area.val())) {
+          return true;
+        }
+        $text_area.val($text_area.val().replace(input_pattern_map.task, ""));
+        return $("#_taskNameInput").focus();
+      }
+    };
+    return $text_area.on("keyup", function() {
+      var pattern;
+      for (pattern in input_pattern_map) {
+        shortcuts[pattern]();
+      }
+    });
+  })();
+
+  (function() {
+    var $chat_buttons, $time_line;
+    $time_line = $('#_timeLine');
+    $chat_buttons = $('#_chatSendTool');
+    $chat_buttons.append('<li id="_bento" role="button" class="_showDescription" aria-label="Bento の集計"><span class="icoSizeLarge" style="font-size: 12px; background: #444; color: #fff; padding: 2px 5px;  -webkit-border-radius: 8px; -moz-border-radius: 8px; border-radius: 10px; font-weight: bold; ">B</span></li>');
+    return $chat_buttons.on('click', '#_bento', function() {
+      var bentos, menu, person, person_names, persons, _i, _len, _results;
+      bentos = {};
+      $time_line.find('._message').each(function() {
+        var matched_array, task_text, _name, _ref;
+        if ($(this).find('._timeStamp') < util.startOfDayTimeStamp()) {
+          return true;
+        }
+        task_text = $(this).find('.chatInfoTaskContentArea').text();
+        matched_array = task_text.match(/([一-龠ー〜ぁ-んァ-ヶ]+)\s*[:：]\s*([一-龠ー〜ぁ-んァ-ヶ]+)/);
+        if (!matched_array) {
+          return true;
+        }
+        bentos[_name = matched_array[2]] || (bentos[_name] = []);
+        if (_ref = matched_array[1], __indexOf.call(bentos[matched_array[2]], _ref) < 0) {
+          return bentos[matched_array[2]].push(matched_array[1]);
+        }
+      });
+      _results = [];
+      for (menu in bentos) {
+        persons = bentos[menu];
+        person_names = [];
+        for (_i = 0, _len = persons.length; _i < _len; _i++) {
+          person = persons[_i];
+          person_names.push("" + person);
+        }
+        _results.push($text_area.val("" + ($text_area.val()) + "\n " + menu + " (" + persons.length + ")：" + (person_names.join(', '))));
+      }
+      return _results;
+    });
+  })();
+
+}).call(this);
